ad3369abc5832bee2e280fe27b9f4e39cb483255
Tommy Persaud <tpersaud27@gmail.com>
2023-01-15 14:57:49 -0500

    Project Setup and Configurations

---
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -0,0 +1,47 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Adding CORS Policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+
+// Serilog Config
+// context is a instance of the builder
+// When we are running the app we can access the console for logs
+builder.Host.UseSerilog((context, loggerConfig) => loggerConfig.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// These are done in the order below
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// This will start logging the requests coming in and the time it takes to complete
+app.UseSerilogRequestLogging();
+
+// Using CORS Policy
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
