faf576c0345b6b4739dc0ccbad76e4ec902898b3
Tommy Persaud <tpersaud27@gmail.com>
2023-01-15 16:05:06 -0500

    Database Modelling and Entity Framework
    Configure models and entity framework. Seeded some initial data.

---
+++ HotelListing.API/HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext: DbContext
+    {
+        /// <summary>
+        /// Note the options listed here are coming from the configuration
+        /// </summary>
+        /// <param name="options"></param>
+        public HotelListingDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Telling entity framework how the data should be when the model is created
+            // This is seeding some initial data into the database
+            modelBuilder.Entity<Country>().HasData(
+                    new Country
+                    {
+                        Id = 1,
+                        Name = "Jamaica",
+                        ShortName= "JM",
+                    },
+                    new Country
+                    {
+                        Id = 2,
+                        Name = "United States of America",
+                        ShortName = "USA"
+                    },
+                    new Country
+                    {
+                        Id = 3,
+                        Name = "Bahamas",
+                        ShortName = "BS"
+                    },
+                    new Country
+                    {
+                        Id = 4,
+                        Name = "Cayman Islands",
+                        ShortName = "CI"
+                    }
+                );
+
+            modelBuilder.Entity<Hotel>().HasData(
+
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryId = 2,
+                    Rating = 4
+                }
+             ); 
+        }
+
+    }
+}
