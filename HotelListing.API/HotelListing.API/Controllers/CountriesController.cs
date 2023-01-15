983e0b7316f4d99351f3d1b8820374dc707e1830
Tommy Persaud <tpersaud27@gmail.com>
2023-01-15 17:27:15 -0500

    Scaffolding API Functionality
    Added CRUD operations for Countries table

---
+++ HotelListing.API/HotelListing.API/Controllers/CountriesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly HotelListingDbContext _context;
+
+        // Injecting DbContext 
+        public CountriesController(HotelListingDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Countries
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+        {
+            // This does this: Select * From Countries
+            var countries = await _context.Countries.ToListAsync();
+            // Return 200 response code
+            return Ok(countries);
+        }
+
+        // GET: api/Countries/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Country>> GetCountry(int id)
+        {
+            // Find the country based on the Id
+            var country = await _context.Countries.FindAsync(id);
+            
+            // If the country does not exist
+            if (country == null)
+            {
+                // Notify the user the country is not found
+                // This is a 404 status code
+                return NotFound();
+            }
+
+            // Return the country found
+            return Ok(country);
+        }
+
+        // PUT: api/Countries/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCountry(int id, Country country)
+        {
+            if (id != country.Id)
+            {
+                return BadRequest("Invalid Record Id");
+            }
+
+            // This will change the state to modified
+            _context.Entry(country).State = EntityState.Modified;
+
+            // If two users are trying to update the record at the same time we should be checking for that. (One user editing record, while one is deleting that same record)
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CountryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            // 204 Status Code
+            return NoContent();
+        }
+
+        // POST: api/Countries
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Country>> PostCountry(Country country)
+        {
+            _context.Countries.Add(country);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+        }
+
+        // DELETE: api/Countries/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCountry(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound("Record does not exist");
+            }
+
+            _context.Countries.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool CountryExists(int id)
+        {
+            return _context.Countries.Any(e => e.Id == id);
+        }
+    }
+}
