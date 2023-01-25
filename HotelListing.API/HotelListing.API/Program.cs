a4e348f2b2c6c2c4fb32b7794fcba955977fd0b1
Tommy Persaud <tpersaud27@gmail.com>
2023-01-24 19:51:18 -0500

    Response Caching
    Add caching for response caching.

--- HotelListing.API/HotelListing.API/Program.cs
+++ HotelListing.API/HotelListing.API/Program.cs
@@ -5,6 +5,8 @@ using HotelListing.API.Middleware;
 using HotelListing.API.Repository;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Serilog;
@@ -41,6 +43,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", b => b.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+});
+
+// Allow Versioning
+builder.Services.AddApiVersioning(options =>
+{
+    // Setting a default version 
+    options.AssumeDefaultVersionWhenUnspecified = true;
+    options.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1, 0);
+    options.ReportApiVersions = true;
+    options.ApiVersionReader = ApiVersionReader.Combine(
+        new QueryStringApiVersionReader("api-version"),
+        new HeaderApiVersionReader("X-Version"),
+        new MediaTypeApiVersionReader("ver")
+    );
 });
 
 
@@ -84,6 +100,14 @@ builder.Services.AddAuthentication(options =>
     };
 });
 
+builder.Services.AddResponseCaching(options =>
+{
+    // Allowing up to 1mb of cached data at any point
+    options.MaximumBodySize = 1024;
+    options.UseCaseSensitivePaths= true;
+});
+
+
 // TODO: Swagger Implementation
 
 var app = builder.Build();
@@ -110,6 +134,31 @@ app.UseHttpsRedirection();
 
 // Using CORS Policy
 app.UseCors("AllowAll");
+
+// Enabling Response caching middleware
+app.UseResponseCaching();
+
+// Middleware code for response cashing
+app.Use(async (context, next) =>
+{
+    // This is letting the user know that the data is coming from the cache rather than fresh data
+    // This is specifying that in the headers
+    context.Response.GetTypedHeaders().CacheControl =
+        new Microsoft.Net.Http.Headers.CacheControlHeaderValue()
+        {
+            // This is saying that this is a public cache of data
+            Public = true,
+            // Keeping the cache alive for 10 seconds. After that fresh data would be available
+            MaxAge = TimeSpan.FromSeconds(10)
+        };
+    // Cache response may vary for the type of the data you can expect
+    context.Response.Headers[Microsoft.Net.Http.Headers.HeaderNames.Vary] =
+        new string[] { "Accept-Encoding" };
+
+    // Carry out the next operation
+    await next();
+});
+
 
 app.UseAuthentication();
 
